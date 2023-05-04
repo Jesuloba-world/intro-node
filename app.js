865d1ce362a873e68fe40635504a0ae8674f43b9
Jesuloba <jesulobajohn@gmail.com>
2023-05-04 12:13:17 +0100

    serving html pages

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
@@ -9,8 +10,13 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
+
+//404 page
+app.use((req, res, next) => {
+	res.status(404).sendFile(path.join(__dirname, "views/404.html"));
+});
 
 app.listen(port);
