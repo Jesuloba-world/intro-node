aaa6d9ed6c45ec4f7f0756113cf59ea773ef10cc
Jesuloba <jesulobajohn@gmail.com>
2023-06-17 19:57:37 +0100

    preparing for templating

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const express = require("express");
 const path = require("path");
+
+const express = require("express");
 const bodyParser = require("body-parser");
 
-const adminRoutes = require("./routes/admin");
+const app = express();
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const app = express();
-const port = 8000;
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
-
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
-//404 page
 app.use((req, res, next) => {
-	res.status(404).sendFile(path.join(__dirname, "views/404.html"));
+	res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
-app.listen(port);
+app.listen(3000);
